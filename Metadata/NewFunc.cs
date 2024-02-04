324de495b6b7896a11c6b6be60fd855784aeed1f
Kye Fenton <kyedylanfenton@gmail.com>
2024-02-04 17:01:11 +1030

    Add spaces after comment slashes

--- Metadata/NewFunc.cs
+++ Metadata/NewFunc.cs
@@ -76,15 +76,15 @@ namespace Metadata
 
 			if (toggle)
 			{
-				//if (!Function.Call<bool>(GTA.Native.Hash._0x36AD3E690DA5ACEB, "CrossLine")) //ANIMPOSTFX_IS_RUNNING                
-				//    Function.Call(GTA.Native.Hash._0x2206BF9A37B7F724, "CrossLine", 0, true); //ANIMPOSTFX_PLAY
-				Function.Call(Hash(0xC8E9B6B71B8E660D), vehicle, toggle, level, power, rechargeTime, disableSound); //_SET_VEHICLE_NITRO_ENABLED           
+				// if (!Function.Call<bool>(GTA.Native.Hash._0x36AD3E690DA5ACEB, "CrossLine")) // ANIMPOSTFX_IS_RUNNING                
+				// Function.Call(GTA.Native.Hash._0x2206BF9A37B7F724, "CrossLine", 0, true); // ANIMPOSTFX_PLAY
+				Function.Call(Hash(0xC8E9B6B71B8E660D), vehicle, toggle, level, power, rechargeTime, disableSound); // _SET_VEHICLE_NITRO_ENABLED           
 			} 
 			else
 			{
-				//if (Function.Call<bool>(GTA.Native.Hash._0x36AD3E690DA5ACEB, "CrossLine")) //ANIMPOSTFX_IS_RUNNING               
-				//    Function.Call(GTA.Native.Hash._0x068E835A1D0DC0E3, "CrossLine"); //ANIMPOSTFX_STOP
-				Function.Call(Hash(0xC8E9B6B71B8E660D), vehicle, toggle, level, power, rechargeTime, disableSound); //_SET_VEHICLE_NITRO_ENABLED
+				// if (Function.Call<bool>(GTA.Native.Hash._0x36AD3E690DA5ACEB, "CrossLine")) // ANIMPOSTFX_IS_RUNNING               
+				// Function.Call(GTA.Native.Hash._0x068E835A1D0DC0E3, "CrossLine"); // ANIMPOSTFX_STOP
+				Function.Call(Hash(0xC8E9B6B71B8E660D), vehicle, toggle, level, power, rechargeTime, disableSound); // _SET_VEHICLE_NITRO_ENABLED
 			}
 		}
 
@@ -92,12 +92,12 @@ namespace Metadata
 		{
 			if (toggle)
 			{
-				Function.Call(GTA.Native.Hash._0x808519373FD336A3, true); //_SET_PLAYER_IS_IN_DIRECTOR_MODE
-				Function.Call(Hash(0x1DFEDD15019315A9), toggle); //_SET_ABILITY_BAR_VISIBILITY_IN_MULTIPLAYER
+				Function.Call(GTA.Native.Hash._0x808519373FD336A3, true); // _SET_PLAYER_IS_IN_DIRECTOR_MODE
+				Function.Call(Hash(0x1DFEDD15019315A9), toggle); // _SET_ABILITY_BAR_VISIBILITY_IN_MULTIPLAYER
 			}
 			else
 			{
-				Function.Call(GTA.Native.Hash._0x808519373FD336A3, false); //_SET_PLAYER_IS_IN_DIRECTOR_MODE
+				Function.Call(GTA.Native.Hash._0x808519373FD336A3, false); // _SET_PLAYER_IS_IN_DIRECTOR_MODE
 			}
 		}
 		#endregion
@@ -212,7 +212,7 @@ namespace Metadata
 
 		public static void SetParachuteModel(this Vehicle vehicle, int modelhash)
 		{
-			//parachute model = 230075693
+			// parachute model = 230075693
 			Function.Call(Hash(0x4D610C6B56031351), vehicle, modelhash);
 		}
 
